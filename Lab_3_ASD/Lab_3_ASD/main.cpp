e738d2c840cdaf1b99ff2add1081381e1d06d686
milka_deluxe <milagalko1@gmail.com>
2021-04-10 22:41:19 +0300

    Geography Class

--- Lab_3_ASD/Lab_3_ASD/main.cpp
+++ Lab_3_ASD/Lab_3_ASD/main.cpp
@@ -1,5 +1,19 @@
 #include "functions.hpp"
 
 int main() {
+    
+    Cities city;
+    bool result = true;
+    
+    for (int i = 0; i < 15; i++) {
+        for (int j = 0; j < 15; j++) {
+            if (city.a_distance[i][j] != city.a_distance[j][i]) {
+                result = false;
+            }
+        }
+    }
+    
+    cout << "Result: " << result << endl;
+    
     return 0;
 }
